ca58d40d9c5a8b8a164020dd96c4cbfdbb323787
Obanijesu <obanijesujohn45@gmail.com>
2023-02-16 02:54:37 +0100

    single page

--- src/App.js
+++ src/App.js
@@ -14,8 +14,8 @@ function App() {
             <Route index element={<Home />} />
             <Route path="login" element={<Login />} />
             <Route path="users">
-              <Route index element={<List />} />
-              <Route path="userId" element={<Single />} />
+              <Route index element={<List/>} />
+              <Route path="userId" element={<Single/>} />
               <Route path="new" element={<New />} />
             </Route>
             <Route path="products">
